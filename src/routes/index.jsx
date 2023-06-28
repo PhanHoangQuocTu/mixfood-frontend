98ca20c9728e647b8d7263d65b4d4218dc3fd774
Tu <tuphan694@gmail.com>
2023-06-28 20:39:10 +0700

    set up project

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+import Home from "@/pages/public/Home"
+
+//public routes
+const publicRoutes = [
+    { path: '/', component: Home },
+]
+
+//private routes
+const privateRoutes = [
+
+]
+
+export { publicRoutes, privateRoutes }
