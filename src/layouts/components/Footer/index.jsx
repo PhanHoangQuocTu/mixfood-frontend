6ad441651812531d22c06a525febfb3cb3249501
Tu <tuphan694@gmail.com>
2023-06-30 18:48:49 +0700

    update FE

--- src/layouts/components/Footer/index.jsx
+++ src/layouts/components/Footer/index.jsx
@@ -10,7 +10,7 @@ function Footer() {
             <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
 
             </section>
-            <section className="">
+            <section className="text-[#fff]">
                 <div className="container text-center text-md-start mt-5">
                     <div className="row mt-3">
                         <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
